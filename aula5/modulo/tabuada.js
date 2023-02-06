c8efbe289f8a83e033ff93c849d2b0059e6de60e
GABRIEL DOMINGUES RIBEIRO <22283347@cfp127aluno.local>
2023-02-06 11:26:18 -0300

    final da aula 5

---
+++ aula5/modulo/tabuada.js
@@ -0,0 +1,45 @@
+/***********************************************************************************************
+ * Arquivo de funções prara calcular uma tabuada de um numero até um contador quanquer
+ * Data 06/02/2023 
+ * Autor: Gabriel Domingues
+ * Versão 1.0 
+ ************************************************************************************************/
+
+
+//Calcula uma tabuada 
+const tabuada = function(multiplicando, maxMultiplicador) {
+    let tabuada = Number(multiplicando);
+    let contador = Number(maxMultiplicador);
+    let contTabuada = 0; //Start da repetição
+    let status = true;
+    let resultado;
+
+    if (tabuada == 0 || contador == 0)
+        status = false;
+    else if (isNaN(tabuada) || isNaN(contador))
+        status = false;
+    else {
+
+        while (contTabuada <= contador) {
+            resultado = tabuada * contTabuada;
+            //2x0=0
+            // console.log(tabuada + 'x' + contTabuada + '=' + resultado);
+            console.log(`${tabuada} x ${contTabuada} = ${resultado}`);
+
+            // contTabuada = contTabuada + 1;
+            contTabuada++;
+            // contTabuada +=1; se voçê trocar o valor do 1 por 2 ou 3 etc, ele vai somar ao valor colocado 
+
+        }
+    }
+
+    return status;
+}
+
+
+tabuada(00100, 3000)
+
+
+module.exports = {
+    tabuada
+}
